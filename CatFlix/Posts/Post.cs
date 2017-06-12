a6cb2178b1be97a3926245d3fba63d4b9c81d2c8
Kavita Sharma <kavita.sharma@sky.uk>
2017-06-12 16:28:35 +0100

    Initial commit

---
+++ CatFlix/Posts/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CatFlix.Posts
+{
+	public sealed class Post
+	{
+		public string Username { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public string Status { get; set; }
+		public string ImageUrl { get; set; }
+		public string AvatarUrl { get; set; }
+	}
+}
---
+++ CatFlix/Posts/PostController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CatFlix.Posts
+{
+	public class PostController : ApiController
+    {
+		public HttpResponseMessage Get()
+		{
+			var posts = new List<Post>();
+
+			posts.Add(new Post
+			{
+				Username = "username",
+				AvatarUrl = "avatar",
+				Status = "Eating lunch",
+				CreatedAt = DateTime.Now,
+				ImageUrl = "sss"
+			});
+
+
+			posts.Add(new Post
+			{
+				Username = "username",
+				AvatarUrl = "avatar",
+				Status = "Eating lunch",
+				CreatedAt = DateTime.Now,
+				ImageUrl = "sss"
+			});
+
+			return Request.CreateResponse(HttpStatusCode.OK, posts);
+		}
+		public HttpResponseMessage Post(Post post)
+		{
+			return Request.CreateResponse(HttpStatusCode.Created, post);
+		}
+
+    }
+}
