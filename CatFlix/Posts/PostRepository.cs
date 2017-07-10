4d7385c9098ee2d8623d825ea55a0334bd54f0a8
Kavita Sharma <kavita.sharma@sky.uk>
2017-07-10 15:23:37 +0100

    Added Post repository

---
+++ CatFlix/Posts/PostRepository.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CatFlix.Posts
+{
+	public class PostRepository
+	{
+		private const string CONNECTION_STRING = "mongodb://localhost:27017";
+		private MongoClient client = new MongoClient(CONNECTION_STRING);
+
+		public async Task Create(Post post)
+		{
+			var database = client.GetDatabase("CatFlix");
+			var collection = database.GetCollection<Post>("Post");
+
+			await collection.InsertOneAsync(post);
+		}
+
+		public async Task<IEnumerable<Post>> Get()
+		{
+			var database = client.GetDatabase("CatFlix");
+			var collection = database.GetCollection<Post>("Post");
+			return await collection.Find(_ => true).ToListAsync();
+		}
+	}
+}
