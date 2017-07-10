d55291ca3faa4328b4b525ecfb52790d1740eee3
Kavita Sharma <kavita.sharma@sky.uk>
2017-07-10 15:20:15 +0100

    Added Id

--- CatFlix/Posts/Post.cs
+++ CatFlix/Posts/Post.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace CatFlix.Posts
 {
 	public sealed class Post
 	{
+		[BsonId]
+		public ObjectId Id { get; set; }
 		public string Username { get; set; }
 		public DateTime CreatedAt { get; set; }
 		public string Status { get; set; }
